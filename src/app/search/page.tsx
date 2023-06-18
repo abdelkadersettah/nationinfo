8f55d6f099568ab031ac6512b1bf0e3d3caad234
Abdelkader <abdelkader.settah@gainchanger.com>
2023-06-18 16:01:37 +0200

    fix the search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,17 +4,17 @@ import CountriesFilter from '@/components/CountriesFilter/CountriesFilter';
 import { CountryDto } from '@/models/CountryDto';
 import { useSearchParams } from 'next/navigation';
 
-export async function getCountriesByName(searchedKey: string) {
-  const countries: Array<CountryDto> = await fetch(
-    `https://restcountries.com/v3.1/name/${searchedKey}`
-  ).then((res) => res.json());
-  return countries;
-}
-
 async function Page() {
   const searchParams = useSearchParams();
   const name = searchParams.get('name');
+  async function getCountriesByName(searchedKey: string) {
+    const countries: Array<CountryDto> = await fetch(
+      `https://restcountries.com/v3.1/name/${searchedKey}`
+    ).then((res) => res.json());
+    return countries;
+  }
   const countries = await getCountriesByName(name ?? '');
+
   return (
     <section>
       <CountriesFilter />
